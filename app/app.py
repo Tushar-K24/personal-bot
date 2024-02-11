92e3346bd86123b6fea63882de3a0a0c3524857e
Tushar Khanduri <62638544+Tushar-K24@users.noreply.github.com>
2024-02-12 00:44:14 +0530

    rm: gino orm + add: sqlalchemy

--- app/app.py
+++ app/app.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
 from app.routers import load_routers
-from app.utils.db_connection import db
 
 
 def get_app() -> FastAPI:
     app: FastAPI = FastAPI()
-    db.init_app(app)
     # Set all CORS enabled origins
     app.add_middleware(
         CORSMiddleware,
